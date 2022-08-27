27a56c29aa0280bfa9dd909fb3d61b200fd782eb
DeVonte G <dagray92@hotmail.com>
2022-08-26 20:00:27 -0400

    2nd commit

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_correct_templates(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'pages/home.html')
